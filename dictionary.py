031949082a7e9ffd383b2a92c287f7f152c82025
Pablo <pablo.kulpa@gmail.com>
2017-04-14 15:34:46 +0200

    added two functions

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,17 @@
+def appellations_alphabeticall(dictionary):
+    key_list = sorted(dictionary.keys())
+    for key in key_list:
+        print(key, end=" ")
+    print('\n')
+
+
 menu_tuple = (0, 1, 2, 3, 4)
 
+dictionary = {'function': ('explanation', 'source'),
+            'parameter': ('explanation', 'source'),
+            'variable': ('explanation', 'source'),
+            'argument': ('explanation', 'source'),
+            'dictionary': ('explanation', 'source')}
 
 print('Welcomme in dictionaryfor a little programmer')
 print('what can i do for you?')
@@ -9,3 +21,11 @@ print('3. show all appellations alphabetically')
 print('4. show available definitions by first letter of appellation')
 print('0. exit')
 menu = int(input('Chose a number: '))
+
+if menu == menu_tuple[1]:
+    appelation_search = input("Write appelation: ")
+    explanation = dictionary[appelation_search]
+    print(explanation[0])
+
+if menu == menu_tuple[3]:  # show all appellations alphabetically
+    appellations_alphabeticall(dictionary)
