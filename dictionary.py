13588c80eca068e272b820c68c438a952251c541
Pablo <pablo.kulpa@gmail.com>
2017-04-14 14:47:20 +0200

    I added menu

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+menu_tuple = (0, 1, 2, 3, 4)
+
+
+print('Welcomme in dictionaryfor a little programmer')
+print('what can i do for you?')
+print('1. search explanation by appellation')
+print('2. add new definition')
+print('3. show all appellations alphabetically')
+print('4. show available definitions by first letter of appellation')
+print('0. exit')
+menu = int(input('Chose a number: '))
