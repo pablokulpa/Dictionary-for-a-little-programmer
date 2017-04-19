2b224a458d454de9cc330a450c07349f430991f8
Pablo <pablo.kulpa@gmail.com>
2017-04-19 11:42:15 +0200

    official

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,15 @@
 import csv
 import sys
+import os.path
 
 
+# checking file existences
+def checking_file(filename):
+    if os.path.exists(filename) is False:
+        sys.exit("Databese file doesnt exist")
+
+
+# reading file and adding to dictionary
 def read(filename, dictionary):
     with open(filename, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -10,6 +18,7 @@ def read(filename, dictionary):
     return dictionary
 
 
+# printing appelations alphabetically
 def appellations_alphabeticall(dictionary):
     key_list = sorted(dictionary.keys())
     for key in key_list:
@@ -17,11 +26,26 @@ def appellations_alphabeticall(dictionary):
     print('\n')
 
 
+# adding to dictionary and saving to file
+def adding(dictionary):
+    new_key = input("Please write definitions: ")
+    new_description = input("Please write description: ")
+    new_source = input("Please write source: ")
+    dictionary[new_key] = new_description, new_source
+    with open('names.csv', 'w', newline='') as csvfile:
+        for key, value in dictionary.items():
+            my_writer = csv.writer(csvfile, delimiter=',')
+            my_writer.writerow([key]+[value[0]]+[value[1]])
+
+
+csvname = 'names.csv'
 dictionary = {}
+checking_file(csvname)
 menu_tuple = (0, 1, 2, 3, 4)
-read('names.csv', dictionary)
+read(csvname, dictionary)
 
 
+# MENU
 print('Welcomme in dictionaryfor a little programmer')
 print('what can i do for you?')
 print('1. search explanation by appellation')
@@ -46,20 +70,15 @@ elif menu == menu_tuple[1]:
     else:
         print("I cant find what you are looking for")
 elif menu == menu_tuple[2]:
-    new_key = input("Please write definitions: ")
-    new_description = input("Please write description: ")
-    new_source = input("Please write source: ")
-    dictionary[new_key] = new_description, new_source
-    with open('names.csv', 'w', newline='') as csvfile:
-        for key, value in dictionary.items():
-            my_writer = csv.writer(csvfile, delimiter=',')
-            my_writer.writerow([key]+[value[0]]+[value[1]])
+    adding(dictionary)
 
-if menu == menu_tuple[3]:  # show all appellations alphabetically
+elif menu == menu_tuple[3]:  # show all appellations alphabetically
     appellations_alphabeticall(dictionary)
 
-if menu == menu_tuple[4]:
+elif menu == menu_tuple[4]:
     first_letter = input("Write first letter of appellations: ")
+    while len(first_letter) > 1:
+        first_letter = input("Write first letter of appellations: ")
     for keys in dictionary.keys():
         if first_letter in keys[0]:
             print(dictionary[keys][0])
